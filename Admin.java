d123a89740b9318056784ddd98b33c541fad32e9
AbelTensay <134728799+AbelTensay@users.noreply.github.com>
2025-01-24 16:24:50 +0300

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,16 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package buyingandselling;
 
-/**
- *
- * @author Abel
- */
+import java.util.List;
+
 public class Admin extends User {
     public Admin(int id, String username, String password) {
         super(id, username, password, "Admin");
+    }
+
+    public void viewAllUsers(UserService userService) {
+        List<User> users = userService.getAllUsers();
+        for (User user : users) {
+            System.out.println("ID: " + user.getId() + ", Username: " + user.getUsername() + ", Role: " + user.getRole());
+        }
+    }
+
+    public void viewAllHouses(HouseService houseService) {
+        List<House> houses = houseService.getAvailableHouses();
+        for (House house : houses) {
+            System.out.println("ID: " + house.getId() + ", Address: " + house.getAddress() + ", Price: $" + house.getPrice() + ", Status: " + house.getStatus());
+        }
+    }
+
+    public void deleteUser(UserService userService, int userId) {
+        userService.deleteUser(userId);
+        System.out.println("User with ID " + userId + " has been deleted.");
+    }
+
+    public void deleteHouse(HouseService houseService, int houseId) {
+        houseService.deleteHouse(houseId);
+        System.out.println("House with ID " + houseId + " has been deleted.");
     }
 }
