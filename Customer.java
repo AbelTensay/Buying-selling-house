8ef5874d45162e5edcfca52426ff3d81552a5763
AbelTensay <134728799+AbelTensay@users.noreply.github.com>
2025-01-24 16:25:27 +0300

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -11,7 +11,21 @@ package buyingandselling;
  */
 
 public class Customer extends User {
-    public Customer(int id, String username, String password) {
+    private String phoneNumber; // New field for phone number
+
+    // Constructor that includes the phone number
+    public Customer(int id, String username, String password, String phoneNumber) {
         super(id, username, password, "Customer");
+        this.phoneNumber = phoneNumber; // Initialize the phone number
+    }
+
+    // Getter for phone number
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    // Setter for phone number
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 }
