9da333331ab30d8a88c231a9fdc1588ffbe777e2
AbelTensay <134728799+AbelTensay@users.noreply.github.com>
2025-01-24 16:32:53 +0300

    Update Owner.java

--- Owner.java
+++ Owner.java
@@ -10,9 +10,26 @@ package buyingandselling;
  * @author Abel
  */
 
-
 public class Owner extends User {
+    private String phoneNumber; // Field to store the owner's phone number
+
     public Owner(int id, String username, String password) {
         super(id, username, password, "Owner");
+    }
+
+    // Constructor with phone number
+    public Owner(int id, String username, String password, String phoneNumber) {
+        super(id, username, password, "Owner");
+        this.phoneNumber = phoneNumber; // Initialize phone number
+    }
+
+    // Getter for phone number
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    // Setter for phone number
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 }
